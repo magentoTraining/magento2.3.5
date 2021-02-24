be4034b3cd0f366ab07016cda55692237de3df92
Shubham Keshri <shubham.keshri@krishtechnolabs.com>
2021-02-24 19:17:17 +0530

    Create Model for GridExample Module

--- app/code/CustomModule/GridExample/Model/ResourceModel/Customer/Collection.php
+++ app/code/CustomModule/GridExample/Model/ResourceModel/Customer/Collection.php
@@ -8,6 +8,10 @@ use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 
 class Collection extends AbstractCollection
 {
+    protected $_idFieldName = 'id';
+    protected $_eventPrefix = 'custommodule_gridexample_collection';
+    protected $_eventObject = 'gridexample_collection';
+
     protected function _construct()
     {
         $this->_init('CustomModule\GridExample\Model\Customer', 'CustomModule\GridExample\Model\ResourceModel\Customer');
